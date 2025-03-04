59b096f3bcf65b08cdb20add819bd9615bbefc51
stephan-carstens <carstensstephan@outlook.com>
2025-03-04 21:27:53 +0200

    TEMP remove vonnevting ti servers. in order to publisch topics to mqtt

--- src/app.py
+++ src/app.py
@@ -73,11 +73,11 @@ class App:
         # if len(servers) == 0: raise RuntimeError(f"No supported servers configured")
 
     def connect(self) -> None:
-        for client in self.clients:
-            client.connect()
+        # for client in self.clients:
+        #     client.connect()
 
-        for server in self.servers:
-            server.connect()
+        # for server in self.servers:
+        #     server.connect()
 
         # Setup MQTT Client
         self.mqtt_client = MqttClient(self.OPTIONS)
